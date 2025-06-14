d7016f51c1611cbb9cc23065973fa84bc9425294
Stepan Tarabin <upconett@gmail.com>
2025-06-14 19:53:15 +0300

    fix: emojis command on Windows
    Closes: #10
    If `platform.system() == "Linux"`, then uses `less`, else prints to stdout.

--- app/cli/actions/emojis.py
+++ app/cli/actions/emojis.py
@@ -2,18 +2,25 @@ import os
 import tempfile
 import subprocess
 
+import platform
+
 from app import emojis
 
 
 def show_emojis_list(): 
     with emojis.unzip() as emoji_dir_path: # type: ignore
         emoji_list = [e.replace(".png", '') for e in os.listdir(emoji_dir_path)]
+    
+    emojis_str = '\n'.join(emoji_list)
 
     with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_file:
-        tmp_file.write('\n'.join(emoji_list))
+        tmp_file.write(emojis_str)
         tmp_file_path = tmp_file.name
 
     try:
-        subprocess.run(["less", tmp_file_path])
+        if platform.system() == "Linux":
+            subprocess.run(["less", tmp_file_path])
+        else:
+            print(emojis_str)
     finally:
         os.unlink(tmp_file_path)
